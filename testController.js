13aa1e0074ed0d67bb8c3598997ac68e89850319
Adam <adam.l.guerra@gmail.com>
2015-09-18 00:19:14 -0500

    still some formatting to do, but it works.

--- testController.js
+++ testController.js
@@ -1,27 +1,34 @@
 angular.module('liveTester',[])
 .controller('codeCtrl', function($scope){
-	$scope.code="function add(a,b){return a+b}"
+	// $scope.code="function add(a,b){return a+b}"
 
 	$scope.runTests = function(){
-		// console.log($scope.code)
 		window.eval($scope.code)
-		// console.log(add(1,2))
+
+		var testCtrl = $scope.$$nextSibling
+		testCtrl.tests.forEach(testCtrl.update)
 	}
-
-
 
 })
 .controller('testCtrl', function($scope){
 
-	console.log('testCtrl')
-	$scope.tests=[{code:'',result='Add a test to get started.'}]
+	$scope.tests=[{code:'',result:'Your test\'s result will be shown here.'}]
 
-	$scope.newTest = function() {$scope.tests.push({code:'',result='Add a test to get started.'})}
+	$scope.newTest = function(item) {
+		var place = $scope.tests.indexOf(item)+1
+		$scope.tests = $scope.tests.slice(0,place).concat({code:'',result:'Your test\'s result will be shown here.'},$scope.tests.slice(place))
+	}
 
 	$scope.result = function(text){
+		try { eval(text) } catch(e) { return 'Error: '+e.message }
 		var testedItem = text.replace(/\s+/g,'').split(/[=<>]+/)[0]
 		return eval(text) === true ? 'True' : 'False!  '+testedItem+' is equal to '+eval(testedItem)
 	}
 
+	$scope.update = function(test){
+		test.result=$scope.result(test.code)
+	}
+
+	$scope.enterCheck = function(keycode,test){if(keycode===13) $scope.update(test)}
 
 })
